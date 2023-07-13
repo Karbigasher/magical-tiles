465a8739477731e558dcd6e2a33ef166132b7911
Karbigasher <nadezhkin.andrey@yandex.ru>
2023-07-13 17:44:28 +0300

    init

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import './magical-movement'
+
+document.querySelector('#image-track').innerHTML = `
+    <img class="image" src="pictures/blackpink-pubg-mobile-pink-background-3413x1920-3105.jpg" draggable="false"/>
+    <img class="image" src="pictures/ferrari-499p-le-mans-hypercar-prototype-2023-world-9504x6336-8997.jpeg" draggable="false"/>
+    <img class="image" src="pictures/gargantua-black-5200x3250-9621.jpg" draggable="false"/>
+    <img class="image" src="pictures/new-york-city-cityscape-night-city-lights-half-moon-5k-5856x3904-437.jpg" draggable="false"/>
+    <img class="image" src="pictures/spider-man-into-the-spider-verse-miles-morales-spider-man-5000x2250-2948.jpg" draggable="false"/>
+`
+
